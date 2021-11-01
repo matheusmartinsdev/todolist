aa1335285a2ece1907dfe20449c11ec750d262dd
Matheus Martins <fmatheusms@gmail.com>
2021-11-01 10:27:24 -0300

    ModelNotFoundException adicionado para registros nao encontrados

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -37,5 +38,16 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $e)
+    {
+        if($e instanceof ModelNotFoundException) {
+            return response()->json([
+                "erro" => "Registro do tipo " .$e->getModel() . " não encontrado."
+            ], 404);
+        }
+
+        return parent::render($request, $e);
     }
 }
