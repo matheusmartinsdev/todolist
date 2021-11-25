bc88c1f072f6f541e559bfd705d8300032678439
Matheus Martins <fmatheusms@gmail.com>
2021-11-24 23:33:40 -0300

    DashboardControllerTest atualizado

--- tests/Feature/App/Http/Controllers/DashboardControllerTest.php
+++ tests/Feature/App/Http/Controllers/DashboardControllerTest.php
@@ -14,19 +14,21 @@ class DashboardControllerTest extends TestCase
     use RefreshDatabase;
 
     /**
-     *
+     * Testa se uma lista de tarefas é fornecida de acordo com filtro 'status' provido
      */
     public function test_wheter_a_task_list_by_status_is_provided()
     {
-        //Creating fake user to test
+        //Criando um usuário fake para testar
         $user = User::factory()->create();
         
         /** @var \Illuminate\Contracts\Auth\Authenticatable $user */
-        //Log in user
+        //Logando o usuário fake criado
         $this->actingAs($user);
+
 
         $taskStatusToBeTested = 'ativa'; //Enum: 'ativa'|'feita'|'cancelada'
 
+        //Criando tarefas com status diversos
         Task::factory()->create([
             'text' => 'tarefa',
             'status' => $taskStatusToBeTested,
@@ -45,7 +47,7 @@ class DashboardControllerTest extends TestCase
             'user_id' => $user->id
         ]);
         
-        //Checking if the $taskStatusToBeTested is founded in the task list.
+        //Checando se apenas a tarefa do tipo $taskStatusToBeTested está na lista
         $response = $this->get('/dashboard?status=' . $taskStatusToBeTested);
         $response->assertInertia(fn (Assert $page) => $page
             ->url('/dashboard?status='. $taskStatusToBeTested)
@@ -56,4 +58,68 @@ class DashboardControllerTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    /**
+     * Testa se as tarefas estão sendo persistidas corretamente.
+     */
+    public function test_if_the_task_is_properly_stored()
+    {
+        //Criando um usuário fake
+        $user = User::factory()->create();
+
+        /** @var \Illuminate\Contracts\Auth\Authenticatable $user */
+        //Logando com o usuário fake
+        $this->actingAs($user);
+
+        //Criando uma tarefa
+        $task = Task::factory()->create([
+            'text' => 'fake task',
+            'status' => 'ativa',
+            'user_id' => $user->id
+        ]);
+
+        //Verificando se a tarefa criada está presente na requisição
+        $response = $this->get('/api/tarefas/' . $task->id);
+        $response->assertOk();
+    }
+
+    /**
+     * Testa se apenas usuários logados podem criar tarefas.
+     */
+    public function test_only_logged_in_users_can_create_tasks()
+    {
+        $task = [
+            'text' => 'tarefa teste',
+            'status' => 'feita',
+            'user_id' => 1
+        ];
+        
+        $response = $this->post('/tarefas/cadastrar', $task);
+        
+        $response->assertRedirect('/login');
+    }
+    
+    /**
+     * Testa se as colunas do model Task estão corretas
+     */
+    public function test_if_task_columns_is_correct()
+    {
+        //Campos esperados ao cadastrar uma tarefa
+        $expectedFillableFields = [
+            'text',
+            'status',
+            'user_id'
+        ];
+
+        //Instanciando Model
+        $task = new Task();
+
+        //Verificando se há diferença entre os campos
+        $arrayCompare = array_diff($expectedFillableFields, $task->getFillable());
+
+        //Assegurando que não há diferença entre os campos
+        $this->assertEquals(0, count($arrayCompare));
+    }
+
+
 }
